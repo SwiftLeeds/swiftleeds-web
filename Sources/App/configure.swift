efb03c8f4d4a1f05f34d36c26a0c96f52fd56d10
Adam Rush <adam.rush@getstream.io>
2021-11-22 17:20:30 +0000

    Introduce Leaf for the frontend.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,14 @@
 import Vapor
+import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // Use Leaf
+    app.views.use(.leaf)
+    
     // register routes
     try routes(app)
 }
