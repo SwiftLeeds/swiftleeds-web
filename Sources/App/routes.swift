ead9e86a5783830b280b4421cc2235bc62abfbf3
Adam Rush <adam.rush@getstream.io>
2022-02-14 18:17:51 +0000

    Creating initial coming soon page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,6 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req -> EventLoopFuture<View> in
-        return req.view.render("Home/home")
+        return req.view.render("Soon/soon")
     }
 }
