99f84d79b8e0bf3516f1ad7879a8db1e2d26904c
Joe Williams <joe.williams@skybettingandgaming.com>
2022-02-22 21:23:51 +0000

    Fix offset and styling

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,6 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req -> EventLoopFuture<View> in
-        return req.view.render("Soon/soon")
+        return req.view.render("Home/home")
     }
 }
