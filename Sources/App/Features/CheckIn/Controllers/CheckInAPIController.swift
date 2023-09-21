b129bfd5039a05f46fa8eb132b538d4763d50cc4
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2023-09-21 14:09:43 +0100

    add checkin tag to API

---
+++ Sources/App/Features/CheckIn/Controllers/CheckInAPIController.swift
@@ -0,0 +1,26 @@
+import Vapor
+
+struct CheckInAPIController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        routes.get(":secret", use: onGet)
+    }
+    
+    func onGet(request: Request) throws -> CheckIn {
+        // Verified that :secret (in the route /api/v1/checkin/:secret) is equal to `CHECKIN_SECRET`
+        // If it is, then return `CHECKIN_TAG`
+        guard
+            let secret = request.parameters.get("secret"),
+            let checkinSecret = Environment.get("CHECKIN_SECRET"),
+            secret == checkinSecret,
+            let tag = Environment.get("CHECKIN_TAG")
+        else {
+            throw Abort(.notFound)
+        }
+
+        return CheckIn(tag: tag)
+    }
+    
+    struct CheckIn: Content {
+        let tag: String
+    }
+}
