9fac7c479e29c39b7882524ebe7b7e5839ac72f6
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2024-01-13 13:32:25 +0000

    Break out static data into separate files
    Just a slight readability enhancement. No intention as of right now to make these database driven due to complexity.

---
+++ Sources/App/Features/Home/CallForPaperFAQs.swift
@@ -0,0 +1,44 @@
+let callForPaperFAQs: [CfpContext.Question] = [
+    .init(
+        question: "What topics can I talk about?",
+        answer: [
+            "It's a long list, but any talks surrounding the Swift language, the ecosystem (such as tooling, Swift Package Manager, Server-side Swift, or other non-Apple platforms), or any Apple operating system are all fair game. We also allow talks on design, architecture, working in software, indie development, testing, and more.",
+            "If in doubt, we would urge you to submit a proposal or discuss it with our team <a href=\"https://join.slack.com/t/swiftleedsworkspace/shared_invite/zt-wkmr6pif-ZDCdDeHM60jcBUy0BxHdCQ\" target=\"_blank\">on Slack</a>."
+        ]
+    ),
+    .init(
+        question: "I've not spoken before, can I apply?",
+        answer: [
+            "Absolutely. We welcome all speakers, whether it's their first time or their hundreth time. We provide optional speaker training for all successful applicants, and will work with you to answer any questions or concerns. We're here to help."
+        ]
+    ),
+    .init(
+        question: "Do I have to pay to talk at SwiftLeeds?",
+        answer: [
+            "You do not need to pay to talk at SwiftLeeds, in fact successful speakers will receive a ticket for both days at SwiftLeeds free of charge. We also pay for two days of accommodation, as well as breakfast and lunch at the venue on both days.",
+            "We will also support travel costs (flights or trains) up to £1,200 GBP or around $1,500 USD, where employers are unable to support."
+        ]
+    ),
+    .init(
+        question: "Are talks recorded or streamed?",
+        answer: [
+            "Yes! All talks at SwiftLeeds are recorded, and streamed live to remote attendees. Videos are uploaded to <a href=\"https://www.youtube.com/@swiftleeds\" target=\"_blank\">our YouTube channel</a> after the event.",
+            "We also photograph every talk too, which are made available on <a href=\"https://www.flickr.com/photos/196979204@N02/\" target=\"_blank\">our Flickr</a>."
+        ]
+    ),
+    .init(
+        question: "I'm dead, blind, or have other accessibility requirments. Can I apply?",
+        answer: [
+            "Absolutely. Let us know by emailing <a href=\"mailto:info@swiftleeds.co.uk\">info@swiftleeds.co.uk</a> and we'll work with you to ensure your needs are met throughout the process and at the venue. Contacting us with your needs will have no impact to the review of your talk proposal."
+        ]
+    ),
+    .init(
+        question: "How do you choose which speakers to have?",
+        answer: [
+            "In 2023 we had 149 talks submitted to our Call for Papers from over 100 speakers, and only 18 slots on the schedule. Choosing which talks to accept takes a lot of time, and is a huge challenge!",
+            "We utilise a tool called <a href=\"https://sessionize.com/\" target=\"_blank\">Sessionize</a> for evaluation of speakers. Our team of more than 10 volunteers each get randomly served three talks at a time and must rank them. At this stage we do not see who the speaker is, but only the title and description you provide.",
+            "This process is repeated 100s of times with many combinations of talks by each of our volunteers until a ranking is generated, at which point we find out who the speakers are.",
+            "With this ranking we work from the top and remove any duplicate talks, and additional talks from the same speaker. We'll then reach out to the top speakers to confirm their attendance, simply working down the list until we have the right number to fill our spots.",
+        ]
+    )
+]
