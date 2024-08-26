1bcb7733b9f7b166e8a9efc2266847a625048526
James Sherlock <james.sherlock@sky.uk>
2024-08-26 15:02:30 +0100

    Fix schedule override

--- Sources/App/Tag/FirstTag.swift
+++ Sources/App/Tag/FirstTag.swift
@@ -3,6 +3,6 @@ import Leaf
 
 struct FirstTag: LeafTag {
     func render(_ ctx: LeafContext) throws -> LeafData {
-        return ctx.parameters.first(where: { !$0.isNil }) ?? .trueNil
+        return ctx.parameters.first(where: { !$0.isNil && $0.double != 0 }) ?? .trueNil
     }
 }
