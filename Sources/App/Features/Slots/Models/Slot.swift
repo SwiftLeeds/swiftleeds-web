212e321ba1eeb09cb23f4b5c9d3f87a5bd0b9e70
Adam Oxley <adam@adamoxley.uk>
2025-07-16 23:30:15 +0100

    Removing unused date and event_id from Slot

--- Sources/App/Features/Slots/Models/Slot.swift
+++ Sources/App/Features/Slots/Models/Slot.swift
@@ -15,16 +15,16 @@ final class Slot: Codable, Model, Content, @unchecked Sendable {
     
     // DO NOT USE (June 2024)
     // TODO: This will be removed in a future PR as part of a cleanup
-    @Field(key: "date")
-    var date: Date?
+//    @Field(key: "date")
+//    var date: Date?
 
     @Field(key: "duration")
     var duration: Double?
 
     // DO NOT USE (June 2024)
     // TODO: This will be removed in a future PR as part of a cleanup - it needs to be done this way for safe migrations.
-    @OptionalParent(key: "event_id")
-    var event: Event?
+//    @OptionalParent(key: "event_id")
+//    var event: Event?
     
     @OptionalParent(key: "day_id")
     var day: EventDay?
@@ -40,23 +40,26 @@ final class Slot: Codable, Model, Content, @unchecked Sendable {
     init(
         id: IDValue?,
         startDate: String,
-        date: Date,
         duration: Double?
     ) {
         self.id = id
         self.startDate = startDate
-        self.date = date
         self.duration = duration
     }
 }
 
 extension Array where Element == Slot {
     var schedule: [[Slot]] {
-        let dates = Set(compactMap { $0.date?.withoutTime }).sorted(by: (<))
+        let dates = Set(compactMap { $0.day?.date.withoutTime }).sorted()
         var slots: [[Slot]] = []
 
         for date in dates {
-            slots.append(filter { Calendar.current.compare(date, to: $0.date ?? Date(), toGranularity: .day) == .orderedSame })
+            slots.append(
+                filter {
+                    guard let slotDate = $0.day?.date else { return false }
+                    return Calendar.current.isDate(slotDate, inSameDayAs: date)
+                }
+            )
         }
 
         return slots
