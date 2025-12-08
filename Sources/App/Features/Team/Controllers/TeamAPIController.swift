2b3c543787c3190d2d0aadee3cd45d71051243a6
James Sherlock <james.sherlock@sky.uk>
2025-12-08 00:04:36 +0000

    update team response

--- Sources/App/Features/Team/Controllers/TeamAPIController.swift
+++ Sources/App/Features/Team/Controllers/TeamAPIController.swift
@@ -13,7 +13,8 @@ struct TeamAPIController: RouteCollection {
                 twitter: "https://twitter.com/Adam9Rush",
                 linkedin: "https://www.linkedin.com/in/swiftlyrush/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/rush.jpg"
+                imageURL: "/img/team/rush.jpg",
+                core: true
             ),
             TeamMember(
                 name: "James Sherlock",
@@ -21,31 +22,35 @@ struct TeamAPIController: RouteCollection {
                 twitter: "https://twitter.com/JamesSherlouk",
                 linkedin: "https://www.linkedin.com/in/jamessherlockdeveloper/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/sherlock.jpg"
+                imageURL: "/img/team/sherlock.jpg",
+                core: true
             ),
             TeamMember(
                 name: "Matthew Gallagher",
-                role: "Registration and Mobile App",
+                role: "Registration",
                 twitter: "https://twitter.com/pdamonkey",
                 linkedin: "https://www.linkedin.com/in/pdamonkey/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D030PN528UA",
-                imageURL: "/img/team/matt.jpg"
+                imageURL: "/img/team/matt.jpg",
+                core: true
             ),
             TeamMember(
                 name: "Adam Oxley",
-                role: nil,
+                role: "iOS Application",
                 twitter: "https://twitter.com/admoxly",
                 linkedin: "https://www.linkedin.com/in/adam-oxley-41183a82/",
                 slack: "https://swiftleedsworkspace.slack.com/team/U02DRL7KUCS",
-                imageURL: "/img/team/oxley.jpg"
+                imageURL: "/img/team/oxley.jpg",
+                core: true
             ),
             TeamMember(
                 name: "Noam Efergan",
-                role: "Camera Operator and Social Media",
+                role: "Production and Social Media",
                 twitter: "https://twitter.com/No_Wham",
                 linkedin: "https://www.linkedin.com/in/noamefergan/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/noam.jpg"
+                imageURL: "/img/team/noam.jpg",
+                core: true
             ),
             TeamMember(
                 name: "Kannan Prasad",
@@ -53,7 +58,8 @@ struct TeamAPIController: RouteCollection {
                 twitter: nil,
                 linkedin: "https://www.linkedin.com/in/kannanprasad/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D0477TRS28G",
-                imageURL: "/img/team/kannan.jpg"
+                imageURL: "/img/team/kannan.jpg",
+                core: false
             ),
             TeamMember(
                 name: "Muralidharan Kathiresan",
@@ -61,7 +67,8 @@ struct TeamAPIController: RouteCollection {
                 twitter: "https://twitter.com/Muralidharan_K",
                 linkedin: "https://www.linkedin.com/in/muralidharankathiresan/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/mural.jpg"
+                imageURL: "/img/team/mural.jpg",
+                core: false
             ),
             TeamMember(
                 name: "Preeti Thombare",
@@ -69,27 +76,32 @@ struct TeamAPIController: RouteCollection {
                 twitter: nil,
                 linkedin: nil,
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/preeti.jpg"
+                imageURL: "/img/team/preeti.jpg",
+                core: false
             ),
             TeamMember(
                 name: "Paul Willis",
-                role: nil,
+                role: "iOS Application",
                 twitter: nil,
                 linkedin: "https://www.linkedin.com/in/paulrobertwillis/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/D05RK6AAV29",
-                imageURL: "/img/team/paul.jpg"
+                imageURL: "/img/team/paul.jpg",
+                core: true
             ),
             TeamMember(
                 name: "Joe Williams",
-                role: "Camera Operator",
+                role: nil,
                 twitter: "https://twitter.com/joedub_dev",
                 linkedin: "https://www.linkedin.com/in/joe-williams-1676b871/",
                 slack: "https://swiftleedsworkspace.slack.com/archives/C05N7JZE2NP",
-                imageURL: "/img/team/joe.jpg"
+                imageURL: "/img/team/joe.jpg",
+                core: false
             ),
         ]
         
         // Shuffle the team members to avoid bias, just like the web page does
-        return TeamResponse(teamMembers: teamMembers.shuffled())
+        let coreTeam = teamMembers.filter { $0.core }.shuffled()
+        let dayTeam = teamMembers.filter { !$0.core }.shuffled()
+        return TeamResponse(teamMembers: coreTeam + dayTeam)
     }
 }
