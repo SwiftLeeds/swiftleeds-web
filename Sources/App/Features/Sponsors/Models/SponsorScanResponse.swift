9464dad97c590d7e87a7be9aaf7380493b5456f0
adamrushy <adamrushy@gmail.com>
2026-04-17 20:39:59 +0100

    Add separation of the name field

--- Sources/App/Features/Sponsors/Models/SponsorScanResponse.swift
+++ Sources/App/Features/Sponsors/Models/SponsorScanResponse.swift
@@ -2,12 +2,16 @@ import Vapor
 
 struct SponsorScanResponse: Content {
     let name: String
+    let firstName: String
+    let lastName: String
     let company: String?
     let email: String
     let responses: [String: String]
 
     init(ticket: TitoTicket) {
         self.name = ticket.fullName
+        self.firstName = ticket.first_name
+        self.lastName = ticket.last_name
         self.company = ticket.company_name
         self.email = ticket.email
         self.responses = ticket.responses
